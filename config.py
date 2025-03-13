4ac69fc5a518f5930ef039752ca1a69d0a4a88ee
krczk <83957500+krczk@users.noreply.github.com>
2025-03-13 13:13:23 +0000

    auth

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 import streamlit as st
 
-load_dotenv()  # Załaduj zmienne z pliku .env
+
 
 # Pobierz klucz API z zmiennych środowiskowych
 API_KEY = st.secrets["API_KEY"]
